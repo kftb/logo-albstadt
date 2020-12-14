c582d26c73792b1c20f15d0dc5eba49b0ebe2758
Kevin Bay <kevin.bay@outlook.com>
2020-12-14 12:35:13 +0100

    Added index.css for fonts

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,77 +1,76 @@
-import * as React from "react"
-import App from '../components/App';
+import * as React from "react";
+import App from "../components/App";
+import "../styles/index.css";
 
 // styles
 const pageStyles = {
-  color: "#232129",
-  padding: "96px",
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
+	color: "#232129",
+	padding: "96px",
+	fontFamily: "-apple-system, Roboto, sans-serif, serif",
+};
 const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
+	marginTop: 0,
+	marginBottom: 64,
+	maxWidth: 320,
+};
 const headingAccentStyles = {
-  color: "#663399",
-}
+	color: "#663399",
+};
 const paragraphStyles = {
-  marginBottom: 48,
-}
+	marginBottom: 48,
+};
 const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
+	color: "#8A6534",
+	padding: 4,
+	backgroundColor: "#FFF4DB",
+	fontSize: "1.25rem",
+	borderRadius: 4,
+};
 const listStyles = {
-  marginBottom: 96,
-  paddingLeft: 0,
-  listStyleType: "none",
-}
+	marginBottom: 96,
+	paddingLeft: 0,
+	listStyleType: "none",
+};
 const listItemStyles = {
-  marginBottom: 12,
-  fontWeight: "300",
-  letterSpacing: 1,
-}
+	marginBottom: 12,
+	fontWeight: "300",
+	letterSpacing: 1,
+};
 const linkStyles = {
-  color: "#8954A8",
-}
+	color: "#8954A8",
+};
 
 // data
 const links = [
-  {
-    text: "Documentation",
-    url: "https://www.gatsbyjs.com/docs/",
-  },
-  {
-    text: "Tutorials",
-    url: "https://www.gatsbyjs.com/tutorial/",
-  },
-  {
-    text: "Guides",
-    url: "https://www.gatsbyjs.com/tutorial/",
-  },
-  {
-    text: "API Reference",
-    url: "https://www.gatsbyjs.com/docs/api-reference/",
-  },
-  {
-    text: "Plugin Library",
-    url: "https://www.gatsbyjs.com/plugins",
-  },
-  {
-    text: "Cheat Sheet",
-    url: "https://www.gatsbyjs.com/docs/cheat-sheet/",
-  },
-]
+	{
+		text: "Documentation",
+		url: "https://www.gatsbyjs.com/docs/",
+	},
+	{
+		text: "Tutorials",
+		url: "https://www.gatsbyjs.com/tutorial/",
+	},
+	{
+		text: "Guides",
+		url: "https://www.gatsbyjs.com/tutorial/",
+	},
+	{
+		text: "API Reference",
+		url: "https://www.gatsbyjs.com/docs/api-reference/",
+	},
+	{
+		text: "Plugin Library",
+		url: "https://www.gatsbyjs.com/plugins",
+	},
+	{
+		text: "Cheat Sheet",
+		url: "https://www.gatsbyjs.com/docs/cheat-sheet/",
+	},
+];
 
 // markup
 const IndexPage = () => {
-  return (
-    <App />
-  )
-}
+	return <App />;
+};
 
-export default IndexPage
+export default IndexPage;
