d1cff27c7be00692bbe209b71cc2b134d2537804
Kevin Bay <kevin.bay@outlook.com>
2020-12-14 17:25:48 +0100

    Added Feature scaffolding

---
+++ src/components/Features/Features.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+import Subfeature from "./Subfeature.js";
+import "../../styles/Features.css";
+
+export default class Features extends Component {
+	render() {
+		return <div className='features'></div>;
+	}
+}
