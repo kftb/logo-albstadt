f175f427f93b2d8c5b80ffdda6be0ee3d10d62a7
Kevin Bay <kevin.bay@outlook.com>
2020-12-14 12:35:51 +0100

    Added bootstrap

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from "react";
 import Navbar from "../components/Navbar";
+import Hero from "../components/Hero/Hero";
+
+import "bootstrap/dist/css/bootstrap.min.css";
 
 export default class App extends Component {
 	render() {
 		return (
 			<div>
 				<Navbar />
+				<Hero />
 			</div>
 		);
 	}
