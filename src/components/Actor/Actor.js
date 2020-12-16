450c6b3a52d9c646d925a90b0ed9b0c7393c51b6
Kevin Bay <kevin.bay@outlook.com>
2020-12-16 08:42:38 +0100

    Added actor with content and rough css

--- src/components/Actor/Actor.js
+++ src/components/Actor/Actor.js
@@ -3,6 +3,24 @@ import "../../styles/Actor.css";
 
 export default class Actor extends Component {
 	render() {
-		return <div></div>;
+		return (
+			<div className='actor'>
+				<div className='actor-container'>
+					<div className='actor-heading'>Ihre Logopädin bei logo albstadt</div>
+					<div className='actor-img'>
+						<img src={require("../../images/vivian.png")} />
+					</div>
+					<div classname='actor-text'>
+						<div className='actor-title'>Vivian Bay</div>
+						<div className='actor-subtitle'>staatlich-anerkannte Logopädin</div>
+					</div>
+					<div className='actor-copy'>
+						I am inspiring dummy text, here to provide an intro to the website.
+						I am inspiring dummy text, here to provide an intro to the website.
+						I am inspiring dummy text, here to provide an intro to the website.
+					</div>
+				</div>
+			</div>
+		);
 	}
 }
