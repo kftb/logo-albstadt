751a3e3b7f1577ff3e3ee884623e3b90922df50e
Kevin Bay <kevin.bay@outlook.com>
2020-12-20 09:04:49 +0100

    Added Contactform page and component

---
+++ src/components/Contactform/Contactform.js
@@ -0,0 +1,81 @@
+import React, { Component } from "react";
+
+import "../../styles/Contactform.css";
+
+export default class Contactform extends Component {
+	handleSubmit() {
+		alert("Vielen Dank für Ihre Nachricht. Wir melden uns umgehend bei Ihnen.");
+	}
+
+	render() {
+		return (
+			<div class='container'>
+				<form class='contact-grid' onSubmit={this.handleSubmit}>
+					<label id='name' for='name'>
+						Vor- und Nachname
+					</label>
+					<input id='namebox' type='text' id='name' placeholder='Ihr Name' />
+
+					<label for='email'>Email-Adresse</label>
+					<input type='email' id='email' placeholder='Ihre Email-Adresse' />
+
+					<label for='phone'>Telefonnummer</label>
+					<input type='tel' id='phone' placeholder='Ihre Telefonnummer' />
+
+					<label for='message'>Nachricht</label>
+					<textarea
+						rows='6'
+						id='message'
+						placeholder='Ihre Nachricht'
+					></textarea>
+
+					<p>Präferierte Tage</p>
+					<ul class='contact-list'>
+						<li>
+							<input type='checkbox' id='Monday' />
+							Montags
+						</li>
+						<li>
+							<input type='checkbox' id='Tuesday' />
+							Dienstags
+						</li>
+						<li>
+							<input type='checkbox' id='wednesday' />
+							Mittwochs
+						</li>
+						<li>
+							<input type='checkbox' id='thursday' />
+							Donnerstags
+						</li>
+						<li>
+							<input type='checkbox' id='friday' />
+							Freitags
+						</li>
+					</ul>
+
+					<p>Präferierte Zeiten</p>
+					<ul class='contact-list'>
+						<li>
+							<input type='checkbox' id='earlymorning' />
+							Früher vormittag (8-10 Uhr)
+						</li>
+						<li>
+							<input type='checkbox' id='latemorning' />
+							Später vormittag (10-12 Uhr)
+						</li>
+						<li>
+							<input type='checkbox' id='earlyafternoon' />
+							Früher nachmittag (12-15 Uhr)
+						</li>
+						<li>
+							<input type='checkbox' id='lateafternoon' />
+							Später nachmittag (15-18 Uhr)
+						</li>
+					</ul>
+
+					<button type='submit'>Kontaktformular absenden</button>
+				</form>
+			</div>
+		);
+	}
+}
---
+++ src/pages/kontakt.js
@@ -0,0 +1,43 @@
+import React, { Component } from "react";
+
+import "../styles/index.css";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer/Footer";
+import Intro from "../components/Intro/Intro";
+import SimpleText from "../components/SimpleText/SimpleText";
+import Process from "../components/Process/Process";
+import Principles from "../components/Principles/Principles";
+import Contactform from "../components/Contactform/Contactform";
+
+export default class Kontaktformular extends Component {
+	render() {
+		return (
+			<div>
+				<Navbar />
+				<br></br>
+				<br></br>
+				<br></br>
+				<SimpleText
+					heading='Kontaktformular'
+					copy={[
+						<p>
+							Schicken Sie uns gerne eine Nachricht über unser Kontaktformular.
+							Wenn Sie nach einem Termin suchen, geben Sie uns gerne Ihre
+							zeitlichen Präferenzen mit und wir suchen nach einem passenden
+							Termin für Sie oder Ihr Kind.{" "}
+						</p>,
+						<p>
+							Ihre Angaben sind selbstverständnlich nicht verpflichtend, helfen
+							uns jedoch Ihnen nur passende Termine vorzuschlagen.
+						</p>,
+					]}
+				/>
+				<Contactform />
+				<Footer />
+			</div>
+		);
+	}
+}
