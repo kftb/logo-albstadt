1286937fa776aa54c97ffa59eee0b731f943a9c8
Kevin Bay <kevin.bay@outlook.com>
2020-12-20 11:53:41 +0100

    Fixed folder path of gatsby-source-filesystem

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,7 +15,7 @@ module.exports = {
 			resolve: "gatsby-source-filesystem",
 			options: {
 				name: "images",
-				path: "./src/images/",
+				path: `${__dirname}/src/images`,
 			},
 			__key: "images",
 		},
@@ -23,7 +23,7 @@ module.exports = {
 			resolve: "gatsby-source-filesystem",
 			options: {
 				name: "pages",
-				path: "./src/pages/",
+				path: `${__dirname}/src/pages`,
 			},
 			__key: "pages",
 		},
