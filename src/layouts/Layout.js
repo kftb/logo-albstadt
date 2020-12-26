32ee2938d74d28ae7ed6f48673b51ab7a8e91685
Kevin Bay <kevin.bay@outlook.com>
2020-12-26 10:50:10 +0100

    Moved layout folder

--- src/layouts/Layout.js
+++ src/layouts/Layout.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
-import Navbar from "../../components/Navbar/Navbar";
-import Footer from "../../components/Footer/Footer";
+import Navbar from "../components/Navbar/Navbar";
+import Footer from "../components/Footer/Footer";
 
 export default class Layout extends Component {
 	render() {
